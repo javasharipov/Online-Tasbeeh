1f6b3cd7c0b269b3f8a94e98ebf3f185db6ee5f4
javasharipov <sharipovjavohir1227@gmail.com>
2024-11-08 22:13:01 +0500

    tasbeeh is finished

---
+++ app.js
@@ -0,0 +1,14 @@
+const count = document.querySelector('.screen__number')
+console.log(count)
+
+let offset = 0
+
+function inc() {
+	offset++
+	count.innerHTML = offset
+}
+
+function reset() {
+	offset = 0
+	count.innerHTML = offset // Update the display to show "0"
+}
